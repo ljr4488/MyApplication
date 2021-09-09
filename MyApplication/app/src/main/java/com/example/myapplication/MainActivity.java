1e5e1643f42208bd2b6624cb9a7b00683ff4d85f
ljr4488 <ljr4488@naver.com>
2021-09-09 10:18:46 +0900

    'pre-push'

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 //
